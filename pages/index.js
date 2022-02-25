7290147a20d1599c9f2fa94c47899b5493bb5bb5
webhome20 <100438451+webhome20@users.noreply.github.com>
2022-02-26 01:20:00 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,101 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+<html>
+<title>
+Visual Hub
+</title>
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+<head>
 
-      <main>
-        <Header title="Web Design and Development Services" />
-        <p className="">
-          Whenever it's about running an online business, it's crucial to manage the website with advanced technicalities. With the web development services in Delhi, you can regulate your web pages and maintain their functionality as web developers design your business page in a way that can be accessed on any device. Ranking the web page on top of the SERP is regulated through search engine optimization techniques. At The Digital Serve, we utilize advanced optimization practices on your website to match with the standard requirements and rank on the top of SERP.
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/style.css">
+
+<script src="js/bootstrap.min.js">
+</script>
 
 
-        </p>
-      </main>
+</head>
 
-      <Footer />
-    </div>
-  )
-}
+
+<body>
+
+<div class="container">
+
+<div class="row">
+
+<div class="col-md-4">
+<img src="image/logo.png" class="logo"> 
+
+
+</div>
+<div class="col-md-8">
+
+<ul>
+<li> Home </li>
+<li> About Us </li>
+<li> Services 
+<ul id="nav"> 
+
+<li> <a href="">Web Designing</a></li>
+<li> <a href="">Web Development</a></li>
+<li> <a href="">Digital Marketing</a></li>
+</ul>
+</li>
+
+<li> Contact Us </li>
+
+</ul>
+</div>
+</div>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<img src="image/web1.jpeg" alt="Quickbooks" style="width: 1280px;height: 400px;margin: 5px 81px;">
+
+<h1>Web Design and Development Services</h1>
+<p>Impex Solutions is primarily Custom Web Design and Development Company. We are experts in providing services such as Website Template Design, Portal Development, Flash Multimedia, brand creation and Web Based Graphics. We have an integrated team of Web Designer, Creative Designers, writers, Web Programmers and Web Marketing Professionals to make website more attractive as well as content rich. Web Designing is an integral part of website development which acts as a major factor in hit-or-flop website. The main driving force behind a perfect website design is its ability to gel with or complement the Company’s objectives, goals. In today’s “Website World” a website’s design reflects the Company’s soul. That is why, the alignment of the design with the Company’s objectives is very important. We understand these mostly ignored aspects of website design and definitely believe that “One Design For All Never Works”.
+
+Based on our customized web design process we are different from typical web design companies. We have built a reputation for creating a lot of web portals for governments as well as in corporate world. So if you want your website developed as per your necessities, this is the right place to come.
+
+We don’t just make websites; we build an image of your company on the internet in such a way that it leaves a very positive impression on any potential client visiting your website. The website in its true sense is like a virtual showroom and the benefits of having an impressive showroom needs no explanation.
+
+Impex Solutions’s greatest strength in web design is in its high quality visualization interface design for the web. The web design firm in India has several highly qualified graphic designers with years of experience developing website interfaces. It has always tried to steer clear of a ‘House Style’, rather providing designs that fit with a client’s identity and that are appropriate for their sector.
+
+All our Multimedia innovations stand out of the crowd, giving your company an edge over your competitors. Our multimedia studio uses high Graphics Computer & Audio systems assuring a time restrained bottom line reducing the web development cost significantly.
+<h2>
+Web Development
+</h2>
+
+As a primary focus of Impex Solutions business strategy, we provide comprehensive on-demand web development solutions for mid-sized businesses. Solution means developing websites that not only have unique look, professional design and easy navigation. Solution also implies delivering websites with integrated application business logic’s, which streamlines your critical business processes.
+
+Specialized in website development, we at Impex Solutions help you execute your ideas and thoughts in the right manner.website development is a mix of art & science which requires a strong understanding of client’s business, the requirement, the brand positioning, the audience and of course huge amount of creativity to integrate all the ideas in a website.
+
+Impex Solutions develops Web Application programs to improve business efficiency by computerizing business transactions, data acquisition, and data management. Impex Solutions has extensive experience in playing on the internet. We develop complete, creative and secure web applications for our vast client base specific to their business needs.
+
+We develop web-based custom applications for businesses needing to streamline daily business operations. Our developers empower our clients by simplifying the Software Development process and product. Our developers code in PHP, Cold fusion and Java using SQL, MYSQL, PostGre and Oracle. Our solutions help minimize redundancy when dealing with customer data, order entry, order tracking and other daily processes.
+
+It is crystal clear that small and home businesses run on limited budgets. That’s why our solutions are affordable and have the highest value/cost ratio. Fully featured small business website will cost starting from $790. And! You will receive exactly what you require, for the price you can afford. Our clientele covers most of small business markets, that’s why a website we develop for you Does Work.</p>
+
+</body>
+</html>
